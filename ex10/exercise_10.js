4bc97c103dc4e5d8197b467739f2475c68a5ffa1
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2023-02-27 22:42:35 +0300

    raiting 706

--- ex10/exercise_10.js
+++ ex10/exercise_10.js
@@ -1,10 +1,20 @@
 "use strict";
 const fs = require("fs");
-const data = fs.readFileSync("input.txt", "utf8").toString().split("\n");
-const inputData = data[0].trim();
+const str = fs.readFileSync("input.txt", "utf8").toString().trim();
 
-const result = resultArr.join("\n");
+const strLen = str.length;
+const abcObj = {};
+for (let i = 0; i < strLen; i++) {
+  Object.hasOwn(abcObj, str[i])
+    ? (abcObj[str[i]] = abcObj[str[i]] + (i + 1) * (strLen - i))
+    : (abcObj[str[i]] = (i + 1) * (strLen - i));
+}
 
-//fs.writeFileSync("output.txt", result + "\n");
+const result = Object.entries(abcObj)
+  .sort()
+  .map((item) => item.join(": "))
+  .join("\n");
+
+fs.writeFileSync("output.txt", result + "\n");
 
 process.stdout.write(result + "\n");
