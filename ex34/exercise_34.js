36902431739f472468ab75b17db060dae60d1e10
SkyLine2R <oleg.vasilenko@gmail.com>
2023-03-10 16:57:26 +0300

    new task

--- ex34/exercise_34.js
+++ ex34/exercise_34.js
@@ -9,7 +9,6 @@ const inputData = fs
   .split("\n");
 const [vertex, edge] = [...inputData.shift().trim().split(" ")];
 const result = [];
-
 inputData.forEach((item, index) => {
   inputData[index] = item.split(" ").map((item) => +item);
 });
@@ -37,20 +36,74 @@ class graph {
     }
   }
   dfs(vertex) {
-    const stack = [[vertex, 0]];
+    const stack = [vertex];
     this.visited[vertex] = 1;
-    let now;
+    let now = vertex;
+    console.log("Заполняем стек");
+    let working = true;
 
-    while (true) {
-      let last = stack.length - 1;
-      if (this.graph[stack[last]]) {
-        //сделать проверку на серость
-        //если норм - добавлять в стек
-        this.graph[stack[last]].forEach;
+    while (working) {
+      let lastElem;
+      this.graph[stack[stack.length - 1]].forEach((item) => {
+        if (!this.visited[item]) {
+          this.visited[item] = 1;
+          stack.push(item);
+        }
+      });
+
+      console.log(lastElem);
+
+      console.log("Состояние стека " + stack);
+      console.log("this.visited" + this.visited);
+      br;
+      if (this.graph[lastElem]) {
+        if (this.visited[lastElem] == 1) {
+          console.log("Обнаружен цикл!");
+          return true;
+        } else working = false;
+        if (this.visited[lastElem] != 2) {
+          this.visited[lastElem] = 1;
+          stack.push(this.graph[lastElem]);
+          //lastElem = this.graph[lastElem];
+        } else break;
+      }
+
+      /* 
+      console.log("lastElem " + lastElem);
+        let element = this.graph[stack[last]];
+        if (element) {
+          console.log(element);
+          this.graph[stack[last]].forEach((item) => {
+            console.log("item " + item);
+            if (this.visited[item] == 1) {
+
+              return true;
+            } else {
+              this.visited[item] = 1;
+              stack.push(item);
+            }
+            if (this.visited[item] == 1) {
+              console.log("Обнаружен тупик");
+            }
+            console.log("Состояние стека " + stack);
+            console.log("this.visited" + this.visited);
+          });
+        }
+      } */
+
+      console.log("Опустошаем стек");
+      while (stack.length) {
+        now = stack.pop();
+        console.log("now " + now);
+        this.visited[now] = 2;
+        this.sortedGraph.push(now);
       }
     }
+    console.log("asdfasdf");
+    console.log(this.graph);
+    console.log("this.sortedGraph " + this.sortedGraph);
 
-    while (stack.length) {
+    /*     while (stack.length) {
       //console.log(this.graph);
       //console.log(stack);
       now = stack.pop();
@@ -68,11 +121,12 @@ class graph {
         });
       }
     }
-    return false;
+    return false; */
   }
   result = "YES";
   graph = [];
   visited = [];
+  sortedGraph = [];
 }
 
 const myGraph = new graph(inputData, vertex);
